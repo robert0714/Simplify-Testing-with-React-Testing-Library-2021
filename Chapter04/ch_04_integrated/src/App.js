fa71692afaf75755d3c8dd0cb6ecf833ca60fddd
robert0714 <robert0714@gmail.com>
2022-05-14 16:55:33 +0800

    adjust ch4 sample

--- Chapter04/ch_04_integrated/src/App.js
+++ Chapter04/ch_04_integrated/src/App.js
@@ -1,3 +1,8 @@
-const App = () => <h1>Replace me with chapter related components </h1>
+import React from 'react';
+// import Vote from './Vote';
+// import Table from './Table';
+import DrinkSearch from './DrinkSearch';
+//const App = () => <h1>Replace me with chapter related components </h1>
+const App = () => <DrinkSearch />
 
 export default App
